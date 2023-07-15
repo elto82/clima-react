1f82e6bf37257b44cccd94338dba9e39bc943ca3
Argiro <elto.82@gmai.com>
2023-07-15 16:29:58 -0500

    obtener datos del form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,21 +1,48 @@
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Formulario = () => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const { ciudad, pais } = busqueda;
+
   useEffect(() => {
     M.AutoInit();
   }, []);
 
+  const handleChange = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="input-field col s12">
-        <input type="text" id="ciudad" name="ciudad" />
+        <input
+          type="text"
+          id="ciudad"
+          name="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
         <label htmlFor="ciudad">Ciudad:</label>
       </div>
       <div className="input-field col s12">
-        <select name="pais" id="pais">
-          <option value="">--Seleccione--</option>
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
+          <option value="">-- Seleccione un país --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
         </select>
         <label htmlFor="pais">Pais:</label>
       </div>
